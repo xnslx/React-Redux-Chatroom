a2f517e224becac7e88eaa5c2e92d44a4ff0f546
xnslx <xnslx89@gmail.com>
2020-08-08 23:10:35 -0500

    Updates

---
+++ precache-manifest.2ecea264c52d010ac34149261ddda111.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d09fc2ab47121fc8670bdb8f0e98cf",
+    "url": "/React-Redux-Chatroom/index.html"
+  },
+  {
+    "revision": "471b24cfb16f56e420a3",
+    "url": "/React-Redux-Chatroom/static/css/main.288a80ee.chunk.css"
+  },
+  {
+    "revision": "e0211b9329b0802305fe",
+    "url": "/React-Redux-Chatroom/static/js/2.e932a933.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-Chatroom/static/js/2.e932a933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471b24cfb16f56e420a3",
+    "url": "/React-Redux-Chatroom/static/js/main.90fc470d.chunk.js"
+  },
+  {
+    "revision": "162aca8ca99ec3ebc4e1",
+    "url": "/React-Redux-Chatroom/static/js/runtime-main.73203897.js"
+  },
+  {
+    "revision": "9f2dcbe50934c35c40eb484f746e4693",
+    "url": "/React-Redux-Chatroom/static/media/ayo-ogunseinde-sibVwORYqs0-unsplash.9f2dcbe5.jpg"
+  },
+  {
+    "revision": "d30290e389c249788836a1a58b784020",
+    "url": "/React-Redux-Chatroom/static/media/christopher-campbell-rDEOVtE7vOs-unsplash.d30290e3.jpg"
+  },
+  {
+    "revision": "245453a451575ea78033a3c5642f9f03",
+    "url": "/React-Redux-Chatroom/static/media/kal-visuals-b1Hg7QI-zcc-unsplash.245453a4.jpg"
+  },
+  {
+    "revision": "de9b37f03f318e679ec4d64e178ec654",
+    "url": "/React-Redux-Chatroom/static/media/michael-dam-mEZ3PoFGs_k-unsplash.de9b37f0.jpg"
+  },
+  {
+    "revision": "c9eb29aef3708d718a855a0554339b3a",
+    "url": "/React-Redux-Chatroom/static/media/philipe-cavalcante-Ugpcxb0jG4Q-unsplash.c9eb29ae.jpg"
+  }
+]);
